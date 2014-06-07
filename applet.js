33fdae0e5084b8f4723da49871ba3947ec8664a3
pixunil <pixunil@gmx.de>
2014-06-07 19:12:16 +0200

    Swap, Graph

--- applet.js
+++ applet.js
@@ -38,8 +38,17 @@ MyApplet.prototype = {
    this.set_applet_tooltip(_("System monitor"));
    
    this.settings = {};
+   this.colors = {};
    this.settingProvider = new Settings.AppletSettings(this.settings, metadata.uuid, instanceId);
    this.settingProvider.bindProperty(Settings.BindingDirection.IN, "interval", "interval");
+   this.settingProvider.bindProperty(Settings.BindingDirection.IN, "width", "width");
+   this.settingProvider.bindProperty(Settings.BindingDirection.IN, "cpu1", "cpu1", Lang.bind(this, this.on_settings_changed));
+   this.settingProvider.bindProperty(Settings.BindingDirection.IN, "cpu2", "cpu2", Lang.bind(this, this.on_settings_changed));
+   this.settingProvider.bindProperty(Settings.BindingDirection.IN, "cpu3", "cpu3", Lang.bind(this, this.on_settings_changed));
+   this.settingProvider.bindProperty(Settings.BindingDirection.IN, "cpu4", "cpu4", Lang.bind(this, this.on_settings_changed));
+   this.settingProvider.bindProperty(Settings.BindingDirection.IN, "mem", "mem", Lang.bind(this, this.on_settings_changed));
+   this.settingProvider.bindProperty(Settings.BindingDirection.IN, "swap", "swap", Lang.bind(this, this.on_settings_changed));
+   this.on_settings_changed();
    
    this.cpu = {
 				gtop: new GTop.glibtop_cpu(),
@@ -57,7 +66,14 @@ MyApplet.prototype = {
    this.mem = {
 				gtop: new GTop.glibtop_mem(),
 				submenu: new PopupMenu.PopupSubMenuMenuItem(_("Memory")),
-				items: [new PopupMenu.PopupMenuItem(_("cached")), new PopupMenu.PopupMenuItem(_("buffered")), new PopupMenu.PopupMenuItem(_("free"))],
+				items: [new PopupMenu.PopupMenuItem(_("used")), new PopupMenu.PopupMenuItem(_("cached")), new PopupMenu.PopupMenuItem(_("buffered"))],
+				container: [new St.BoxLayout()]
+			};	
+			
+			this.swap = {
+				gtop: new GTop.glibtop_swap(),
+				submenu: new PopupMenu.PopupMenuItem(_("Swap")),
+				//items: [new PopupMenu.PopupMenuItem(_("used")), new PopupMenu.PopupMenuItem(_("cached")), new PopupMenu.PopupMenuItem(_("buffered"))],
 				container: [new St.BoxLayout()]
 			};	
 			this.data = {
@@ -66,7 +82,8 @@ MyApplet.prototype = {
 					system: [],
 					user: []
 				},
-				mem: {}
+				mem: {},
+				swap: {}
 			};		
 		 this.menuManager = new PopupMenu.PopupMenuManager(this);
 			this.menu = new Applet.AppletPopupMenu(this, orientation);
@@ -99,8 +116,14 @@ MyApplet.prototype = {
 			 this.mem.submenu.menu.addMenuItem(this.mem.items[i]);
 			}
 			this.menu.addMenuItem(this.mem.submenu);
-			this.canvas = new St.DrawingArea();
-			//this.canvas.connect('repaint', Lang.bind(this, this.draw));
+			
+			this.swap.container[0].add_actor(new St.Label({text: "? MiB", width: 100, style: "text-align: right"}));
+   this.swap.container[0].add_actor(new St.Label({text: "?%", width: 60, style: "text-align: right"}));
+   this.swap.submenu.addActor(this.swap.container[0]);
+			this.menu.addMenuItem(this.swap.submenu);
+			
+			this.canvas = new St.DrawingArea({height: 150});
+			this.canvas.connect('repaint', Lang.bind(this, this.draw));
    item = new PopupMenu.PopupBaseMenuItem({reactive: false});
 		 item.addActor(this.canvas, {span: -1, expand: true});
 		 this.menu.addMenuItem(item);
@@ -139,9 +162,14 @@ MyApplet.prototype = {
 			
 			GTop.glibtop_get_mem(this.mem.gtop);
 		 this.data.mem.total = Math.round(this.mem.gtop.total / 1024 / 1024);
+		 this.data.mem.used = Math.round(this.mem.gtop.used / 1024 / 1024);
 		 this.data.mem.buffer = Math.round(this.mem.gtop.buffer / 1024 / 1024);
 		 this.data.mem.cached = Math.round(this.mem.gtop.cached / 1024 / 1024);
-		 this.data.mem.usedup = Math.round(this.mem.gtop.used / 1024 / 1024) - this.data.mem.buffer - this.data.mem.cached;
+		 this.data.mem.usedup = this.data.mem.used - this.data.mem.buffer - this.data.mem.cached;
+
+   GTop.glibtop_get_swap(this.swap.gtop);
+   this.data.swap.total = Math.round(this.swap.gtop.total / 1024 / 1024);
+		 this.data.swap.used = Math.round(this.swap.gtop.used / 1024 / 1024);
 		} catch(e){
 			global.logError(e);
 		}
@@ -152,14 +180,33 @@ MyApplet.prototype = {
 			let w = this.canvas.get_width();
 			let h = this.canvas.get_height();
 			
-			let r = 10;
-			ctx.setLineWidth(5);
-			for(let i = 0, l = this.cpu.usage.length; i < l; ++i){
-				ctx.setSourceRGBA(this.cpu.colors[i][0], this.cpu.colors[i][1], this.cpu.colors[i][2], 1);
-				ctx.arc(w / 2, h / 2, r, -Math.PI / 2, Math.PI * this.cpu.usage[i] / 50 - Math.PI / 2);
+			let r = 20, a = -Math.PI / 2;
+   function arc(angle, dir){
+				if(dir) ctx.arc(w / 2, h / 2, r, a, a += angle);
+				else ctx.arcNegative(w / 2, h / 2, r, a, a -= angle);
 				ctx.stroke();
-			 r += 5; 
 			}
+			ctx.setLineWidth(this.settings.width);
+			for(let i = 0; i < this.cpu.count; ++i){
+				ctx.setSourceRGBA(this.colors["cpu" + (i % 4 + 1)][0], this.colors["cpu" + (i % 4 + 1)][1], this.colors["cpu" + (i % 4 + 1)][2], 1);
+				arc(Math.PI * this.data.cpu.user[i] * 2, true);
+				ctx.setSourceRGBA(this.colors["cpu" + (i % 4 + 1)][0], this.colors["cpu" + (i % 4 + 1)][1], this.colors["cpu" + (i % 4 + 1)][2], .8);
+				arc(Math.PI * this.data.cpu.system[i] * 2, true);
+				ctx.stroke();
+			 r += this.settings.width;
+			 a = -Math.PI / 2; 
+			}
+			ctx.setSourceRGBA(this.colors["mem"][0], this.colors["mem"][1], this.colors["mem"][2], 1);
+			arc(Math.PI * this.data.mem.usedup / this.data.mem.total * 2, false);
+			ctx.setSourceRGBA(this.colors["mem"][0], this.colors["mem"][1], this.colors["mem"][2], .8);
+			arc(Math.PI * this.data.mem.cached / this.data.mem.total * 2, false);
+			ctx.setSourceRGBA(this.colors["mem"][0], this.colors["mem"][1], this.colors["mem"][2], .6);
+			arc(Math.PI * this.data.mem.buffer / this.data.mem.total * 2, false);
+			r += this.settings.width;
+			a = -Math.PI / 2;
+			
+			ctx.setSourceRGBA(this.colors["swap"][0], this.colors["swap"][1], this.colors["swap"][2], 1);
+			arc(Math.PI * this.data.swap.used / this.data.swap.total * 2, false);
 	 } catch(e){
 			global.logError(e);
 		}
@@ -172,13 +219,20 @@ MyApplet.prototype = {
 				this.cpu.container[1].get_children()[i].set_text(Math.round(this.data.cpu.user[i] * 100) + "%");
 				this.cpu.container[2].get_children()[i].set_text(Math.round(this.data.cpu.system[i] * 100) + "%");
    }
-			this.mem.container[0].get_children()[0].set_text(this.data.mem.usedup + " MiB");
-			this.mem.container[0].get_children()[1].set_text(Math.round(1000 * this.data.mem.usedup / this.data.mem.total) / 10 + "%");
-   this.mem.container[1].get_children()[0].set_text(this.data.mem.buffer + " MiB");
-			this.mem.container[1].get_children()[1].set_text(Math.round(1000 * this.data.mem.cached / this.data.mem.total) / 10 + "%");
-   this.mem.container[2].get_children()[0].set_text(this.data.mem.cached + " MiB");
-			this.mem.container[2].get_children()[1].set_text(Math.round(1000 * this.data.mem.buffer / this.data.mem.total) / 10 + "%");
-   //this.canvas.queue_repaint();
+
+			this.mem.container[0].get_children()[0].set_text(this.data.mem.used + " MiB");
+			this.mem.container[0].get_children()[1].set_text(Math.round(1000 * this.data.mem.used / this.data.mem.total) / 10 + "%");
+			this.mem.container[1].get_children()[0].set_text(this.data.mem.usedup + " MiB");
+			this.mem.container[1].get_children()[1].set_text(Math.round(1000 * this.data.mem.usedup / this.data.mem.total) / 10 + "%");
+   this.mem.container[2].get_children()[0].set_text(this.data.mem.buffer + " MiB");
+			this.mem.container[2].get_children()[1].set_text(Math.round(1000 * this.data.mem.cached / this.data.mem.total) / 10 + "%");
+   this.mem.container[3].get_children()[0].set_text(this.data.mem.cached + " MiB");
+			this.mem.container[3].get_children()[1].set_text(Math.round(1000 * this.data.mem.buffer / this.data.mem.total) / 10 + "%");
+			
+			this.swap.container[0].get_children()[0].set_text(this.data.swap.used + " MiB");
+			this.swap.container[0].get_children()[1].set_text(Math.round(1000 * this.data.swap.used / this.data.swap.total) / 10 + "%");
+			
+   this.canvas.queue_repaint();
 			if(this.menu.isOpen) Mainloop.timeout_add(this.settings.interval,	Lang.bind(this, this.refresh));
 		} catch(e){
 			global.logError(e);
@@ -187,7 +241,19 @@ MyApplet.prototype = {
 	on_applet_clicked: function(event){
 		this.menu.toggle();
 		if(this.menu.isOpen) this.refresh();
- }
+ },
+ on_settings_changed: function(){
+		try {
+   ["cpu1", "cpu2", "cpu3", "cpu4", "mem", "swap"].forEach(function(p){
+		 	let c = this.settings[p].split(","), i;
+		  for(let i = 0; i < 3; ++i)
+		   c[i] = parseInt(c[i].match(/\d+/)) / 255;
+	  	this.colors[p] = c;
+		 }, this);
+	 } catch(e){
+			global.logError(e);
+		}
+	}
 };
 
 
